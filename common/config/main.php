1bdd482c5599e248559a693a958f8c5978acc2c5
JJVAAMONDE <jose120293@gmail.com>
2017-02-09 19:47:16 -0400

    nada
    fallido el intento

--- common/config/main.php
+++ common/config/main.php
@@ -1,4 +1,5 @@
 <?php
+
 return
 [
 
@@ -22,7 +23,7 @@ return
       [
         'class' => 'yii\debug\Module',
       ],
-    
+
         'datecontrol' =>
          [
             'class' => 'kartik\datecontrol\Module',
